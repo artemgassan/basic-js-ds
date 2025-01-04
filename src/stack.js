c53fcfdfc08647b77e53544a8974247fe419607c
artemgassan <artemgassan@ya.ru>
2025-01-04 16:02:58 +0300

    feat: finish Stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.mySteck = [];
+  }
+  push(element) {
+    this.mySteck.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.mySteck.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.mySteck[this.mySteck.length - 1];
   }
 }
 
